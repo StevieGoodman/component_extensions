927dfb2b59af39712a331b1c995e9d3f126edb55
Stevie Goodman <mail@steviegoodman.com>
2024-10-20 22:08:43 +0100

    Fixed incorrect path

--- src/init.lua
+++ src/init.lua
@@ -4,7 +4,7 @@ ComponentExtensions.IsClass = require(script.IsClass)
 ComponentExtensions.RequiresComponent = require(script.RequiresComponent)
 ComponentExtensions.RequiresInstance = require(script.RequiresInstance)
 ComponentExtensions.RequiresAttribute = require(script.RequiresAttribute)
-ComponentExtensions.GetInstance = require(script.GetInstance)
+ComponentExtensions.GetInstances = require(script.GetInstances)
 ComponentExtensions.GetComponents = require(script.GetComponents)
 
 return ComponentExtensions
